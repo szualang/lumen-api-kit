27782cafa009aae2eff6dd54bf386da94bdbb45c
szualang <liangalang@qq.com>
2018-01-22 16:35:55 +0800

    首次提交项目

---
+++ routes/api/v1.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: szualang
+ * Date: 2018-1-22
+ * Time: 15:46
+ */
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api\V1',
+    'middleware' => [
+    'cors',
+    //'serializer',
+    //'serializer:array', // if you want to remove data wrap
+    'api.throttle',
+    ],
+    // each route have a limit of 200 of 1 minutes
+    'limit' => 200, 'expires' => 1,], function ($api){
+    // 以下是项目api路由设定
+
+});
